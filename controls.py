3bfd40bb19c14d32f1f3204d9073e240da0c3a05
ninja1001 <57629995+ninja1001@users.noreply.github.com>
2022-08-19 23:50:10 +0400

    .

---
+++ controls.py
@@ -0,0 +1,129 @@
+import pygame, sys
+from bullet import Bullet
+from alien import Alien
+import time
+
+
+
+
+def events(screen, gun, bullets):
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                #right
+                if event.key == pygame.K_d:
+                    gun.mright = True
+                #left
+                elif event.key == pygame.K_a:
+                    gun.mleft = True
+                elif event.key == pygame.K_SPACE:
+                    new_bulet = Bullet(screen, gun)
+                    bullets.add(new_bulet)
+            elif event.type == pygame.KEYUP:
+                #right
+                if event.key == pygame.K_d:
+                    gun.mright = False
+                #left
+                elif event.key == pygame.K_a:
+                    gun.mleft = False
+                    
+                    
+def update(bg_color, screen, stats, sc, gun, aliens, bullets):
+    
+    screen.fill(bg_color)
+    sc.show_scope()
+    for bullet in bullets.sprites():
+        bullet.drow_bullet()
+    gun.output()
+    aliens.draw(screen)
+    pygame.display.flip()
+    
+def update_bullets(screen, stats, sc, aliens, bullets):
+    
+    bullets.update()
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+    #print(len(bullets))
+    
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+    
+    if collisions:
+        
+        for aliens in collisions.values():
+            
+        
+            stats.scope += 10 * len(aliens)
+        sc.image_score()
+        check_high_score(stats, sc)
+        #sc.image_guns()
+    if len(aliens) == 0:
+        bullets.empty()
+        create_army(screen, aliens)
+     
+     
+     
+     
+def gun_kill(stats, screen, sc, gun, aliens, bullets):
+    
+    
+    if stats.guns_left > 0:
+        stats.guns_left -= 1
+        #sc.image_guns()
+        aliens.empty()
+        bullets.empty()
+        create_army(screen, aliens)
+        gun.create_gun()
+        time.sleep(2)
+    else:
+        stats.run_game = False
+        sys.exit()
+     
+     
+def update_aliens(stats, screen, sc, gun, aliens, bullets):
+    
+    aliens.update()
+    if pygame.sprite.spritecollide(gun, aliens, True):
+        gun_kill(stats, screen, sc, gun, aliens, bullets)
+    aliens_check(stats, screen, sc,  gun, aliens, bullets)
+
+def aliens_check(stats, screen, sc, gun, aliens, bullets):
+    
+    screen_rect = screen.get_rect()
+    
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            gun_kill(stats, screen, sc, gun, aliens, bullets)
+            break
+        
+    
+                
+                
+def create_army(screen, aliens):
+    alien = Alien(screen)
+    alien_width = alien.rect.width
+    number_alien_x = int((1000-2 * alien_width) / alien_width)
+    alien_height = alien.rect.height
+    number_alien_y = int((1000 - 100 - 2*alien_height)/alien_height)
+    
+    for row_number in range(number_alien_y - 3):
+        
+        for alien_num in range(number_alien_x):
+            alien = Alien(screen)
+            alien.x = alien_width + alien_width * alien_num
+            alien.y = alien_height + alien_height * row_number
+            alien.rect.x = alien.x
+            alien.rect.y = alien.rect.height +  alien.rect.height * row_number
+            aliens.add(alien)
+            
+            
+            
+def check_high_score(stats, sc):
+    
+    
+    if stats.scope > stats.high_score:
+        stats.high_score = stats.scope
+        sc.image_high_score()
+        with open('high_score.txt', 'w') as f:
+            f.write(str(stats.high_score))
